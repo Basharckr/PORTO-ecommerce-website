bd212b801040b9acc93a55211cdd975a190c1c10
basharckr <basharpmr@gmail.com>
2021-04-23 16:01:48 +0530

    final

--- porto/urls.py
+++ porto/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('', include('myapp.urls')),
     path('vendor/', include('vendor.urls')),
     path('owner/', include('owner.urls')),
+    
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
